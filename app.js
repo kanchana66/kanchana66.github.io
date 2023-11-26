a44d80ca4364e2ae28d0a93c2ad86379c4fd9b73
kanchana66 <ict20066@std.uwu.ac.lk>
2023-11-26 13:57:36 +0530

    Update mouse pointer and navigation bar

--- app.js
+++ app.js
@@ -62,3 +62,47 @@ tags.forEach(btn => {
         btn.classList.add('active');
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const cursor = document.createElement("div");
+    cursor.classList.add("custom-cursor");
+    document.body.appendChild(cursor);
+
+    document.addEventListener("mousemove", function (e) {
+        cursor.style.left = e.clientX + "px";
+        cursor.style.top = e.clientY + "px";
+    });
+
+    const hoverElements = document.querySelectorAll(
+        "a, button, .project-card, .filter-btn, .btn"
+    );
+
+    hoverElements.forEach((element) => {
+        element.addEventListener("mouseenter", () => {
+            cursor.style.width = "40px";
+            cursor.style.height = "40px";
+        });
+
+        element.addEventListener("mouseleave", () => {
+            cursor.style.width = "20px";
+            cursor.style.height = "20px";
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Your existing code...
+
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", function () {
+        const scrolled = window.scrollY;
+
+        if (scrolled === 0) {
+            // At the top of the page
+            navbar.classList.remove("hidden");
+        } else {
+            // Scrolled down
+            navbar.classList.add("hidden");
+        }
+    });
+});
